f9e0ea01b3dd9480f71c51e94ee1d69f03febc29
TatjanaLukic <id4tanja@gmail.com>
2020-01-25 15:18:58 +0100

    Updates

---
+++ precache-manifest.aaf8f0d3aed5e841a3f5c0b071fa176a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4871e5c2cad4c800fe44e33f5ae44c4e",
+    "url": "/wikipedia-search/index.html"
+  },
+  {
+    "revision": "14c7aa2e1b943fdd08e2",
+    "url": "/wikipedia-search/static/js/2.82450798.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wikipedia-search/static/js/2.82450798.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d44f83d24f8be17aaa2",
+    "url": "/wikipedia-search/static/js/main.f3bdb7e6.chunk.js"
+  },
+  {
+    "revision": "bf31fa2e43b47805bff8",
+    "url": "/wikipedia-search/static/js/runtime-main.89a80443.js"
+  }
+]);
